5db087a5bbc8e7b0e3c2bd50d46e3b71a50a5d71
Pavel Nosach <103734995+chemiic@users.noreply.github.com>
2023-09-30 01:17:13 +0300

    Update addProduct.ts

--- actions/Api/addProduct.ts
+++ actions/Api/addProduct.ts
@@ -4,7 +4,7 @@ import {Product} from "../../types/types";
 type addProductProps = Omit<Product, 'id'>
 const addProduct =  async (product:addProductProps) => {
     try {
-        const res  = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}`, product);
+        const res  = await axios.post(`https://marshy-foamy-fenugreek.glitch.me/api/goods`, product);
         return {type: 'response', res: res.status}
     } catch (error) {
         return {type: 'error', res: error}
