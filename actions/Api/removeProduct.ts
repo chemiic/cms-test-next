b9e109a8023b059a4531293984e41092c6d84e6b
Pavel Nosach <103734995+chemiic@users.noreply.github.com>
2023-09-30 01:19:35 +0300

    Update removeProduct.ts

--- actions/Api/removeProduct.ts
+++ actions/Api/removeProduct.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const removeProduct =  async (id:string) => {
     try {
-        const res  = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL + '/' + id}`);
+        const res  = await axios.delete(`https://marshy-foamy-fenugreek.glitch.me/api/goods/${id}`);
         return {type: 'response', res: res.status}
     } catch (e) {
         return {type: 'error', res: e}
