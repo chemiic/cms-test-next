39fb38e40be3aa8b69ecaadef69bb33930342b71
Pavel Nosach <103734995+chemiic@users.noreply.github.com>
2023-09-30 01:17:24 +0300

    Update editProduct.ts

--- actions/Api/editProduct.ts
+++ actions/Api/editProduct.ts
@@ -5,7 +5,7 @@ import {Product} from "../../types/types";
 
 const editProduct = async (product:Product) => {
     try {
-        const res  = await axios.patch(`${process.env.NEXT_PUBLIC_API_URL + '/' + product.id}`, product);
+        const res  = await axios.patch(`${https://marshy-foamy-fenugreek.glitch.me/api/goods + '/' + product.id}`, product);
         return {type: 'response', res: res.status}
     } catch (error) {
         return {type: 'error', res: error}
