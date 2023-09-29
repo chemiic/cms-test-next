47e50f2452c7b62d7886383b1c18d681adbf8bf1
chemiic <nosach.pavel@yandex.ru>
2023-09-30 01:22:59 +0300

    fix #3

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,27 @@ import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import getProducts from "../actions/Api/getProducts";
 
-
 const HomePage: NextPage =  async () => {
-
-        const products = await getProducts()
-        return (
-            <>
-                <ToastContainer
-                    position="top-right"
-                    autoClose={2000}
-                    hideProgressBar={false}
-                    newestOnTop
-                    closeOnClick
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover={false}
-                    theme="light"
-                    className={`absolute top-10 right-10 max-w-[300px]`}
-                />
-                <Header products={products}/>
-                <Table products={products}/>
-            </>
-        );
-    }
+    const products = await getProducts()
+    return (
+        <>
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover={false}
+                theme="light"
+                className={`absolute top-10 right-10 max-w-[300px]`}
+            />
+            <Header products={products}/>
+            <Table products={products}/>
+        </>
+    );
+}
 ;
 export default HomePage;
